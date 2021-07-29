ebf210a68969e081f98fdecf31352c834cd4e1f6
Mehul Singh Rathore <mehulsingh072001@gmail.com>
2021-07-29 13:49:15 +0530

    fixed token auth problem

--- server/app/Http/Controllers/CalendarController.php
+++ server/app/Http/Controllers/CalendarController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Google;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Cookie;
 
 class CalendarController extends Controller
 {
@@ -13,12 +13,11 @@ class CalendarController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function connect()
+    public function connect(Request $request)
     {
         $credentialsPath = storage_path('keys/client_secret_917561590462-mjm99i957992qb6na831cbaq98vtlfbj.apps.googleusercontent.com.json');
         $client = new Google\Client();
         $client->setAuthConfig($credentialsPath);
-        $client->setClientId('917561590462-mjm99i957992qb6na831cbaq98vtlfbj.apps.googleusercontent.com');
         $client->addScope(Google\Service\Calendar::CALENDAR);
         $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/api/redirect');
         $client->setAccessType('offline');        // offline access
@@ -34,9 +33,43 @@ class CalendarController extends Controller
         $client = new Google\Client();
         $client->setAuthConfig($credentialsPath);
         $client->addScope(Google\Service\Calendar::CALENDAR);
-        echo $_GET['code'];
 
-        echo implode($client->fetchAccessTokenWithAuthCode($_GET['code']));
+        $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+        $client->setAccessToken($accessToken['access_token']);
+
+        $event = new Google\Service\Calendar\Event(array(
+          'summary' => 'Google I/O 2015',
+          'location' => '800 Howard St., San Francisco, CA 94103',
+          'description' => 'A chance to hear more about Google\'s developer products.',
+          'start' => array(
+            'dateTime' => '2015-05-28T09:00:00-07:00',
+            'timeZone' => 'America/Los_Angeles',
+          ),
+          'end' => array(
+            'dateTime' => '2015-05-28T17:00:00-07:00',
+            'timeZone' => 'America/Los_Angeles',
+          ),
+          'recurrence' => array(
+            'RRULE:FREQ=DAILY;COUNT=2'
+          ),
+          'attendees' => array(
+            array('email' => 'lpage@example.com'),
+            array('email' => 'sbrin@example.com'),
+          ),
+          'reminders' => array(
+            'useDefault' => FALSE,
+            'overrides' => array(
+              array('method' => 'email', 'minutes' => 24 * 60),
+              array('method' => 'popup', 'minutes' => 10),
+            ),
+          ),
+        ));
+
+        $calendarId = 'primary';
+        $service = new Google\Service\Calendar($client);
+
+        $event = $service->events->insert($calendarId, $event);
+        echo 'Event created: %s\n', $event->htmlLink;
     }
 
 
