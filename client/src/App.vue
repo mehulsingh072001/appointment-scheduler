fc378124450d3ee7e1d9e3afbd1397f45e5d36b4
Mehul Singh Rathore <mehulsingh072001@gmail.com>
2021-08-20 20:46:22 +0530

    dates by days classification successful

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <DatePicker/>
   </div>
 </template>
@@ -17,12 +16,18 @@ export default {
 </script>
 
 <style>
+body{
+  background-color: #2c3e50;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+button{
+  background-color: transparent;
+  color: white;
 }
 </style>
--- client/src/components/DatePicker.vue
+++ client/src/components/DatePicker.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <button>Click</button>
-    <div v-for="day in days" v-bind:key="day.id">
-      <button id="day">{{day}}</button>
+    <div v-for="date in dates" v-bind:key="date.id">
+      <button id="day">{{date}}</button>
     </div>
   </div>
 </template>
@@ -13,23 +13,73 @@ export default {
       var d = new Date()
       this.month = d.getMonth()
       this.year = d.getFullYear()
-      this.getDaysInMonth(this.month, this.year)
+      this.getDatesInMonth(this.month, this.year)
+      this.getDaysOfDates()
   },
+
   data(){
     return{
       month: "",
       year: "",
-      days: []
+      dates: [],
+      sun: [],
+      mon: [],
+      tue: [],
+      wed: [],
+      thu: [],
+      fri: [],
+      sat: []
     }
   },
+
   methods:{
-    getDaysInMonth: function(month, year) {
+    getDatesInMonth: function(month, year) {
       var date = new Date(year, month);
       while(date.getMonth() === month){
-        this.days.push(new Date(date));
+        this.dates.push(new Date(date));
         date.setDate(date.getDate() + 1);
       }
-      console.log(this.days)
+    },
+    getDaysOfDates: function(){
+       for (let i = 0; i < this.dates.length; i++) {
+         var date = new Date(this.dates[i])
+         var date_to_str = date.toString()
+         var strings = []
+         strings.push(date_to_str.trim().split(" "));
+         for(let i = 0; i < strings.length; i++){
+           if(strings[i][0] == 'Mon'){
+             this.mon.push(strings[i][2])
+           }
+           if(strings[i][0] == 'Tue'){
+             this.tue.push(strings[i][2])
+           }
+           if(strings[i][0] == 'Wed'){
+             this.wed.push(strings[i][2])
+           }
+           if(strings[i][0] == 'Thu'){
+             this.thu.push(strings[i][2])
+           }
+           if(strings[i][0] == 'Fri'){
+             this.fri.push(strings[i][2])
+           }
+           if(strings[i][0] == 'Sat'){
+             this.sat.push(strings[i][2])
+           }
+           if(strings[i][0] == 'Sun'){
+             this.sun.push(strings[i][2])
+           }
+           else{
+             console.log(false)
+           }
+         }
+      } 
+      console.log(this.sun)
+      console.log(this.mon)
+      console.log(this.tue)
+      console.log(this.wed)
+      console.log(this.thu)
+      console.log(this.fri)
+      console.log(this.sat)
     }
   },
 }
